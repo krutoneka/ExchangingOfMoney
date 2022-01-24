9e7eede48b89190bbf941a5b2fad12579d0a86c0
krutoneka <krutoneka3@gmail.com>
2022-01-24 18:40:55 +0300

    4StepOfBotWorking

---
+++ money_check/bot.py
@@ -0,0 +1,23 @@
+import telebot
+import config
+import pb
+import datetime
+import pytz
+import json
+import traceback
+
+P_TIMEZONE = pytz.timezone(config.TIMEZONE)
+TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
+
+bot = telebot.TeleBot(config.TOKEN)
+bot.polling(non_stop=True)
+
+@bot.massage_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(
+        message.chat.id,
+        'Greetings! I can show you exchange rates.\n' +
+        'To get the exchange rates press /exchange.\n' +
+        'To get help press /help.'
+    )
+
