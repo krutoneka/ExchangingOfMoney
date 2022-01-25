7f90df50ab55a79fab90cacf5c47daeab7f24136
krutoneka <krutoneka3@gmail.com>
2022-01-25 22:23:22 +0300

    4StepOfBotWorking(#2)

--- money_check/bot.py
+++ money_check/bot.py
@@ -10,9 +10,9 @@ P_TIMEZONE = pytz.timezone(config.TIMEZONE)
 TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
 
 bot = telebot.TeleBot(config.TOKEN)
-bot.polling(non_stop=True)
+bot.polling(none_stop=True)
 
-@bot.massage_handler(commands=['start'])
+@bot.message_handler(commands=['start'])
 def start_command(message):
     bot.send_message(
         message.chat.id,
@@ -20,4 +20,39 @@ def start_command(message):
         'To get the exchange rates press /exchange.\n' +
         'To get help press /help.'
     )
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.add(
+        telebot.types.InlineKeyboardButton(
+            'Message the developer', url='https://t.me/psix1337'
+  )
+    )
+    bot.send_message(
+        message.chat.id,
+        '1) To receive a list of available currencies press /exchange.\n' +
+        '2) Click on the currency you are interested in.\n' +
+        '3) You will receive a message containing information regarding the source and the target currencies, ' +
+        'buying rates and selling rates.\n' +
+        '4) Click “Update” to receive the current information regarding the request. ' +
+        'The bot will also show the difference between the previous and the current exchange rates.\n' +
+        '5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.',
+        reply_markup=keyboard
+    )
 
+@bot.message_handler(commands=['exchange'])
+def exchange_command(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(
+        telebot.types.InlineKeyboardButton('USD', callback_data='get-USD')
+    )
+    keyboard.row(
+        telebot.types.InlineKeyboardButton('EUR', callback_data='get-EUR'),
+        telebot.types.InlineKeyboardButton('RUB', callback_data='get-RUB')
+    )
+
+    bot.send_message(
+        message,chat.id,
+        'Click on the currency of choice:',
+        reply_markup=keyboard
+    )
