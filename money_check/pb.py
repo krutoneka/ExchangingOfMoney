14419946ffc428d003bead2b151a10b404e763ee
krutoneka <krutoneka3@gmail.com>
2022-01-26 20:52:32 +0300

    4StepOfBotWorking(#3)

--- money_check/pb.py
+++ money_check/pb.py
@@ -4,16 +4,19 @@ import json
 
 URL = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
 
+
 def load_exchange():
     return json.loads(requests.get(URL).text)
 
+
 def get_exchange(ccy_key):
     for exc in load_exchange():
-        if ccy_key ==exc['ccy']:
+        if ccy_key == exc['ccy']:
             return exc
     return False
 
-def get_exchanges(ccy_patterns):
+
+def get_exchanges(ccy_pattern):
     result = []
     ccy_pattern = re.escape(ccy_pattern) + '.*'
     for exc in load_exchange():
